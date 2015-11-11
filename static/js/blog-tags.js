50c275329ebca875c7a32dd4642ac787090ced06
Momin Khan <foolmoron@gmail.com>
2015-11-10 21:27:12 -0500

    Hash-based filtering on blog page

--- static/js/blog-tags.js
+++ static/js/blog-tags.js
@@ -1,37 +1,47 @@
 (function() {
-  var tags, tagFilters, taggedItems;
-
+  var tagFilters, taggedItems;
+  tagFilters = $('[data-tag]');
+  taggedItems = $('[data-tags]');
+  
   // setup tag filtering
   {
-  	tags = [];
-    tagFilters = $('[data-tag]');
-    taggedItems = $('[data-tags]');
-
     tagFilters.click(function(e) {
-    	var tagFilter = $(e.currentTarget);		
-    	var tag = tagFilter.data('tag');
-    	if (tags.indexOf(tag) >= 0) {
-    		tags.splice(tags.indexOf(tag), 1);
-    		tagFilter.removeClass('active');
-    	} else {
-    		tags.push(tag);
-    		tagFilter.addClass('active');
-    	}
+      var tagsToFilterBy = location.hash.substr(1).split(',').map(function(tag) { return tag.trim(); });
 
-    	if (tags.length == 0) {
-    		taggedItems.removeClass('hidden');
-    	} else {
-    		for (var i = 0; i < taggedItems.length; i++) {
-    			var item = taggedItems.eq(i);
-    			var itemTags = item.data('tags');
+      var tagFilter = $(e.currentTarget);
+      var tag = tagFilter.data('tag');
 
-    			var matchingTag = false;
-    			for (var t = 0; t < tags.length; t++) {
-    				matchingTag |= itemTags.indexOf(tags[t]) >= 0;
-    			};
-    			item.toggleClass('hidden', !matchingTag);
-    		};
-    	}
+      tagFilter.toggleClass('active', tagsToFilterBy.indexOf(tag) < 0)
+      location.hash = tagFilters.filter('.active').get().map(function(item) { return $(item).data('tag'); }).join(',');
+    });
+  }
+
+  // read hash to determine filters
+  {
+    $(window).on('hashchange load', function() {
+      var tagsToFilterBy = location.hash.substr(1).split(',').map(function(tag) { return tag.trim(); });
+
+      if (tagsToFilterBy.length == 0) {
+        tagFilters.removeClass('active');
+        taggedItems.removeClass('hidden');
+      } else {
+        // activate filter buttons based on tags
+        tagFilters.removeClass('active')
+        for (var t = 0; t < tagsToFilterBy.length; t++) {
+          tagFilters.filter('[data-tag="' + tagsToFilterBy[t] + '"]').addClass('active');
+        };
+        // hide tagged items based on tags
+        for (var i = 0; i < taggedItems.length; i++) {
+          var item = taggedItems.eq(i);
+          var itemTags = item.data('tags');
+
+          var matchingTag = false;
+          for (var t = 0; t < tagsToFilterBy.length; t++) {
+            matchingTag |= itemTags.indexOf(tagsToFilterBy[t]) >= 0;
+          };
+          item.toggleClass('hidden', !matchingTag);
+        };
+      }
     });
   }
 })()
