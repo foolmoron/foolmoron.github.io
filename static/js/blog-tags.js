2b978d81319fe8d042c8ac325cb66f70a5d55a7b
Momin Khan <foolmoron@gmail.com>
2015-11-10 21:43:19 -0500

    Fading tagged items in and out

--- static/js/blog-tags.js
+++ static/js/blog-tags.js
@@ -18,30 +18,40 @@
 
   // read hash to determine filters
   {
-    $(window).on('hashchange load', function() {
-      var tagsToFilterBy = location.hash.substr(1).split(',').map(function(tag) { return tag.trim(); });
+    var fadeTaggedItemsGenerator = function(fadeSpeed) {
+      return function() {
+        var tagsToFilterBy = location.hash.substr(1).split(',').map(function(tag) { return tag.trim(); });
 
-      if (tagsToFilterBy.length == 0) {
-        tagFilters.removeClass('active');
-        taggedItems.removeClass('hidden');
-      } else {
-        // activate filter buttons based on tags
-        tagFilters.removeClass('active')
-        for (var t = 0; t < tagsToFilterBy.length; t++) {
-          tagFilters.filter('[data-tag="' + tagsToFilterBy[t] + '"]').addClass('active');
-        };
-        // hide tagged items based on tags
-        for (var i = 0; i < taggedItems.length; i++) {
-          var item = taggedItems.eq(i);
-          var itemTags = item.data('tags');
-
-          var matchingTag = false;
+        tagFilters.stop();
+        if (tagsToFilterBy.length == 0) {
+          tagFilters.removeClass('active');
+          taggedItems.fadeIn(fadeSpeed);
+        } else {
+          // activate filter buttons based on tags
+          tagFilters.removeClass('active')
           for (var t = 0; t < tagsToFilterBy.length; t++) {
-            matchingTag |= itemTags.indexOf(tagsToFilterBy[t]) >= 0;
+            tagFilters.filter('[data-tag="' + tagsToFilterBy[t] + '"]').addClass('active');
           };
-          item.toggleClass('hidden', !matchingTag);
-        };
-      }
-    });
+          // hide tagged items based on tags
+          for (var i = 0; i < taggedItems.length; i++) {
+            var item = taggedItems.eq(i);
+            var itemTags = item.data('tags');
+
+            var matchingTag = false;
+            for (var t = 0; t < tagsToFilterBy.length; t++) {
+              matchingTag |= itemTags.indexOf(tagsToFilterBy[t]) >= 0;
+            };
+            if (matchingTag) {
+              item.fadeIn(fadeSpeed);
+            } else {
+              item.fadeOut(fadeSpeed);
+            }
+          };
+        }
+      };
+    };
+
+    $(window).on('load', fadeTaggedItemsGenerator(0)); // make transitions instant on load
+    $(window).on('hashchange', fadeTaggedItemsGenerator(300)); // and smoother after that
   }
 })()
